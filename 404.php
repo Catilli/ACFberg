510a6efb0e695a239915bc6d74d3754c8444d0bc
Catilli <hello.cath.dev@gmail.com>
2025-08-08 22:45:43 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -16,6 +16,7 @@ defined( 'ABSPATH' ) || exit;
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title('|', true, 'right'); ?></title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <?php wp_head(); ?>
 </head>
 
